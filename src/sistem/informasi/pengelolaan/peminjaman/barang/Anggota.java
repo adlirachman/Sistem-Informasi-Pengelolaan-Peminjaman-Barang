c43fb8060ab8f9e859ab17c14e0cf709f2cfea0f
Muhammad Adli Rachman <adli.rahman23@gmail.com>
2017-02-26 18:55:21 +0700

    First

---
+++ src/sistem/informasi/pengelolaan/peminjaman/barang/Anggota.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistem.informasi.pengelolaan.peminjaman.barang;
+
+/**
+ *
+ * @author Adli Rachman
+ */
+public class Anggota extends Orang {
+    private String idAnggota;
+    
+}

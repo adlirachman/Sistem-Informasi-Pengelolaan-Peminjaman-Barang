8c4e0c88bbe4a27ae9a8352a0705dbcda5bb6235
Muhammad Adli Rachman <adli.rahman23@gmail.com>
2017-02-27 17:26:05 +0700

    Nambahin attribut barang

--- src/sistem/informasi/pengelolaan/peminjaman/barang/Barang.java
+++ src/sistem/informasi/pengelolaan/peminjaman/barang/Barang.java
@@ -10,5 +10,33 @@ package sistem.informasi.pengelolaan.peminjaman.barang;
  * @author Adli Rachman
  */
 public class Barang {
+    private String nama;
+    private int jumlah;
+    private String kode;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public int getJumlah() {
+        return jumlah;
+    }
+
+    public void setJumlah(int jumlah) {
+        this.jumlah = jumlah;
+    }
+
+    public String getKode() {
+        return kode;
+    }
+
+    public void setKode(String kode) {
+        this.kode = kode;
+    }
+    
     
 }
